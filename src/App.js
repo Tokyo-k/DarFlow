f9e64c91458fc36a699d5223b7eac4acd3e10937
Tokyo / 토키오 <173747846+Tokyo-k@users.noreply.github.com>
2024-10-19 21:17:29 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import IntegrationForm from './components/IntegrationForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>DarkFlow - Automatización e Integración</h1>
+      <IntegrationForm />
+    </div>
+  );
+}
+
+export default App;
